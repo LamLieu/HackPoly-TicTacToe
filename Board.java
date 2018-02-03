b30dec5d0415432e881a256985388f88c6642c95
Lam Lieu <lieulam@hotmail.com>
2018-02-03 14:36:56 -0800

    Added Board and Player classes.

---
+++ Board.java
@@ -0,0 +1,15 @@
+public class Board {
+   private char[][] board;
+
+   public Board() {
+      board = new char[3][3];
+   }
+
+   public int[][] updateBoard() {
+
+   }
+
+   public int[][] getBoard() {
+      return board;
+   }
+}
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,22 @@
-public class TicTacToe {
+import java.util.Scanner;
 
+public class TicTacToe {
+   public static void main(String[] args) {
+      String input;
+      Scanner kb = new Scanner(System.in);
+      System.out.println("Welcome to the Tic Tac Toe Game!");
+      System.out.print("Pick your character X or O (type X or O): ");
+      input = kb.nextLine();
+      if (input.charAt(0) == 'X') {
+
+      }
+      else if (input.charAt(0) == 'O') {
+
+      }
+      else {
+         System.out.println("Error: Please enter either X or O.");
+
+      }
+
+   }
 }
